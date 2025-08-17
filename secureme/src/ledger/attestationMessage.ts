ed29ef3e4589a58c7bf332732a718c2c9f2ba552
Ishan Pathak <ishan.pathak2711@gmail.com>
2025-08-17 01:43:17 -0400

    Name change issue for deleted files

--- secureme/src/ledger/attestationMessage.ts
+++
@@ -1,5 +0,0 @@
-export function formatAttestationMessage(nonceHex: string): string {
-  const clean = nonceHex.startsWith("0x") ? nonceHex : `0x${nonceHex}`;
-  return `SecureMe session attestation\nNonce: ${clean}`;
-}
-
